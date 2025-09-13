5e97efd1a09d60d0bf42054d714cda96ecd0559a
ラワンアウンピョウ <hlumaungphyo@gmail.com>
2025-09-13 18:30:07 +0900

    Initial Commit

---
+++ Genetic Algorithm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Genetic Algorithm
+//
+//  Created by cmStudent on 2025/09/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Genetic Algorithm/Genetic_AlgorithmApp.swift
@@ -0,0 +1,17 @@
+//
+//  Genetic_AlgorithmApp.swift
+//  Genetic Algorithm
+//
+//  Created by cmStudent on 2025/09/13.
+//
+
+import SwiftUI
+
+@main
+struct Genetic_AlgorithmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
